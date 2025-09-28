7c0b8c2f928873ddc021871f5b586a6a57b3dc6e
Carson Smith <csm2356@wgu.edu>
2025-09-28 03:34:17 -0500

    Created Time class and implemented it int Window. Added update method in Window

--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,39 @@
 import javax.swing.JFrame;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class Window extends JFrame implements Runnable {
+
+    Graphics2D g2;
+
     public Window() {
         this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
         this.setTitle(Constants.TITLE);
         this.setResizable(false);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g2 = (Graphics2D)this.getGraphics();
+    }
+
+    public void update(double dt) {
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
     }
 
     public void run() {
+        double lastFrameTime = 0.0;
         while (true) {
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+
+            try{
+                Thread.sleep(30);
+            } catch(InterruptedException e) {
+
+            }
 
         }
     }
